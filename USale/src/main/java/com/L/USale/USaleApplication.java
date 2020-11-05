25178ca5b2e84079004ad4bad7cf5e7fa04a3395
minyiche <m770706946@gmail.com>
2020-11-04 23:05:41 -0800

    Initial commit

---
+++ USale/src/main/java/com/L/USale/USaleApplication.java
@@ -0,0 +1,13 @@
+package com.L.USale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class USaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(USaleApplication.class, args);
+	}
+
+}
---
+++ USale/src/test/java/com/L/USale/USaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.L.USale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class USaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
