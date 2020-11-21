ea2ec3471fd6b8e5792b7b8f55f241285115fb0d
Zhuojun Chen <zhuojunc@usc.edu>
2020-11-21 12:01:37 +0800

    User API With Model

--- USale/src/main/java/com/L/USale/controller/UserController.java
+++ USale/src/main/java/com/L/USale/controller/UserController.java
@@ -3,6 +3,10 @@ package com.L.USale.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +15,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.L.USale.entity.User;
+import com.L.USale.entity.UserLogin;
 import com.L.USale.service.UserService;
 import com.L.USale.validator.UserValidator;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 	
@@ -22,45 +27,60 @@ public class UserController {
 	UserService userService;
 	
     @Autowired
-    UserValidator userValidator;	
+    UserValidator userValidator;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(Model model) {
+        model.addAttribute("message", "hello");
+        return "welcome";
+    }
 	
-	@RequestMapping(value = "/search", method = RequestMethod.GET)
-	@ResponseBody
-	public List<User> getUser(@RequestParam(name = "userName", required=true) String userName, @RequestParam(name = "password", required=true) String password) {
-		List<User> users = userService.searchUser(userName, password);
-		return users;
-	}	
-	
-	@RequestMapping(value = "/update", method = RequestMethod.PUT)
-	public boolean updateUser(@RequestBody User user) {
-		try {
-			userService.updateUser(user);
-			return true;
-		}catch(Exception e) {
-			return false;
-		}
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String searchUser(Model model) {
+		UserLogin userLogin = new UserLogin();
+		model.addAttribute("userLogin", userLogin);
+		return "login";
 	}
 	
-	@RequestMapping(value = "create", method = RequestMethod.POST)
-	public boolean postUser(@RequestBody User user) {
-		try {
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String searchUser(@ModelAttribute("userLogin") UserLogin userLogin, Model model) {
+		boolean found = userService.searchUser(userLogin.getUserName(), userLogin.getPassword());
+		if(found) {
+			return "homepage";
+		}
+		model.addAttribute("message", "username or password incorrect.");
+		return "login";
+	}
+	
+	@RequestMapping(value = "/update", method = RequestMethod.PUT)
+	public String updateUser(@ModelAttribute("user") User user) {			
+		userService.updateUser(user);
+		return "account";
+	}
+	
+	@RequestMapping(value="/signup", method = RequestMethod.GET)
+	public String createUser(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "signup";
+	}
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String createUser(@ModelAttribute("user") User user, Model model) {
 			if (userValidator.validate(user)) {
 				userService.createUser(user);
-				return true;
+				model.addAttribute("message", "Sign Up Successfully!");
+				return "login";
 			}
-			return false;
-		}catch(Exception e) {
-			return false;
-		}
+			else {
+				model.addAttribute("message", "Username is taken. Try another one.");
+				return "signup";
+			}
 	}	
 	
 	@RequestMapping(value = "delete", method = RequestMethod.DELETE)
-	public boolean deleteUser(@RequestParam(name = "id", required=true) int id) {
-		try {
+	public String deleteUser(@RequestParam(name = "id", required=true) int id) {
 			userService.deleteUser(id);
-			return true;
-		}catch(Exception e) {
-			return false;
-		}
+			return "login";
 	}	
 }
